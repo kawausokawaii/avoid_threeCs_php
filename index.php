af2931ed7063e32b80fd36f08abde760afe105db
nssol_Minori.Kawasaki <kawasaki.minori@nano.ns-sol.co.jp>
2022-02-08 21:13:15 +0900

    phpの体裁を整える

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset='utf-8'>
+<title>3密見守りくん</title>
+</head>
+<body>
+<h1><?php echo "三密見守りくん"; ?></h1><br>
 <img src="img/yuriko.jpg" alt="yuriko" title="百合子が3密について何か言っている">
 <br><br>
 <?php
-
-echo "Hello World!";
+echo "3密よ！";
+?>
+</body>
+</html>
