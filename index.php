e50990c527216342db65cad7c62345f90f236a2c
nssol_Minori.Kawasaki <kawasaki.minori@nano.ns-sol.co.jp>
2022-02-28 15:05:48 +0900

    DB接続

--- index.php
+++ index.php
@@ -77,6 +77,34 @@
         </niko>
     </h1>
 
+    <?php
+        // Initialize connection variables.
+        $host = "bunkakai-postgre-db.postgres.database.azure.com";
+        $database = "bunkakai-postgre-db";
+        $user = "dbadmin";
+        $password = "OP8dev_flkU";
+
+        // Initialize connection object.
+        $connection = pg_connect("host=$host dbname=$database user=$user password=$password") 
+            or die("Failed to create connection to database: ". pg_last_error(). "<br/>");
+        print "Successfully created connection to database.<br/>";
+
+        // Perform some SQL queries over the connection.
+        $query = "SELECT * from co2";
+        $result_set = pg_query($connection, $query) 
+            or die("Encountered an error when executing given sql statement: ". pg_last_error(). "<br/>");
+        while ($row = pg_fetch_row($result_set))
+        {
+            print "Data row = ($row). <br/>";
+        }
+
+        // Free result_set
+        pg_free_result($result_set);
+
+        // Closing connection
+        pg_close($connection);
+    ?>
+
     <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
     <script type="text/javascript" src="main.js"></script>
 
