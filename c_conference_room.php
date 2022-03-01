742486751692f48595f00904f1c96a6ef16917c6
nssol_Minori.Kawasaki <kawasaki.minori@nano.ns-sol.co.jp>
2022-03-01 16:56:50 +0900

    popup機能を追加

--- c_conference_room.php
+++ c_conference_room.php
@@ -7,11 +7,12 @@
 </head>
 <body>
 
+    <link href="https://use.fontawesome.com/releases/v5.0.8/css/all.css" rel="stylesheet">
     <link rel="stylesheet" href="main.css">
     <link href="https://fonts.googleapis.com/earlyaccess/nicomoji.css" rel="stylesheet">
 
     <div class="navigation">
-    <!-- 階層に分けてナビゲーションの骨組みを作成 -->
+    <!-- 会議室ナビゲーション -->
         <ul>
             <li class="button">
             <p><a href="index.php">会議室A</a></p>
@@ -25,6 +26,7 @@
         </ul>
     </div>
 
+    <!-- 会議室見出し -->
     <div class="c">
         <h2>
             会議室C
@@ -32,36 +34,58 @@
     </div>
 
     <?php
-        // Initialize connection variables.
+        // DB接続情報
         $host = "bunkakai-postgre-db.postgres.database.azure.com";
         $database = "bunkakai";
         $user = "dbadmin";
         $password = "OP8dev_flkU";
         $port = "5432";
 
-        // Initialize connection object.
+        // DB接続
         $connection = pg_connect("host=$host port=$port dbname=$database user=$user password=$password sslmode=disable")
         or die("Failed to create connection to database: ". pg_result_error(). "<br/>");
         
+        $co2 = 0;
+        $count = 0;
+        $decibel = 0;
+
+        // CO2の取得
+        $query = "SELECT * from co2 ORDER BY send_time DESC";
+        $result_set = pg_query($connection, $query) 
+            or die("Encountered an error when executing given sql statement: ". pg_last_error(). "<br/>");
+        while ($row = pg_fetch_row($result_set))
+        {
+            $co2 = $row[1];
+            break;
+        }
+
+        // 人数の取得(残件)
+
+        // 音量の取得(残件)
+
+
+        // 3つの密を満たしたらポップアップを表示する
+        if($co2 > 1000 && $count > 3 && $decibel > 80) :
+    ?>
+            <div class="popup" id="js-popup">
+                <div class="popup-inner">
+                    <div class="close-btn" id="js-close-btn"><i class="fas fa-times"></i></div>
+                    <a href="#"><img src="./yuriko.jpg" alt="yuriko"></a>
+                </div>
+                <div class="black-background" id="js-black-bg"></div>
+            </div>
+    <?php
+        endif;
     ?>
 
     <section class="strips">
 
+        <!-- 密閉 -->
         <article class="strips__strip">
             <div class="strip__content">
                 <h1><niko>密閉</niko></h1>
                 <h1 class="strip__title" data-name="Lorem">
-                    <?php
-                        // Perform some SQL queries over the connection.
-                        $query = "SELECT * from co2 ORDER BY send_time DESC";
-                        $result_set = pg_query($connection, $query) 
-                            or die("Encountered an error when executing given sql statement: ". pg_last_error(). "<br/>");
-                        while ($row = pg_fetch_row($result_set))
-                        {
-                            print "$row[1]ppm";
-                            break;
-                        }
-                    ?>
+                    <?php echo "$co2 ppm";?>
                 </h1>
                 <div class="strip__inner-text">
                     <h2>Co2</h2>
@@ -70,30 +94,36 @@
             </div>
         </article>
 
+        <!-- 密集 -->
         <article class="strips__strip">
             <div class="strip__content">
                 <h1><niko>密集</niko></h1>
-                <h1 class="strip__title" data-name="Ipsum">3/5人</h1>
+                <h1 class="strip__title" data-name="Ipsum"><?php echo "$count/5人";?></h1>
                 <div class="strip__inner-text">
-                    <h2>Co2</h2>
+                    <h2>在室者一覧</h2>
+                    A<br>
+                    B
+                </div>
+            </div>
+        </article>
+
+        <!-- 密接 -->
+        <article class="strips__strip">
+            <div class="strip__content">
+                <h1><niko>密接</niko></h1>
+                <h1 class="strip__title" data-name="Dolor"><?php echo "$decibel dB";?></h1>
+                <div class="strip__inner-text">
+                    <h2>音量</h2>
                     <iframe src="https://bunkakai-grafana.azurewebsites.net/d-solo/-H9M00fnk/test?orgId=1&from=1645432439100&to=1645432559246&panelId=2" width="450" height="200" frameborder="0"></iframe>
                 </div>
             </div>
         </article>
 
-        <article class="strips__strip">
-        <div class="strip__content">
-        <h1><niko>密接</niko></h1>
-        <h1 class="strip__title" data-name="Dolor">100dB</h1>
-        <div class="strip__inner-text">
-        <h2>Co2</h2>
-            <iframe src="https://bunkakai-grafana.azurewebsites.net/d-solo/-H9M00fnk/test?orgId=1&from=1645432439100&to=1645432559246&panelId=2" width="450" height="200" frameborder="0"></iframe>
-        </div>
-        </div>
-        </article>
+        <!-- 詳細のcloseボタン -->
         <i class="fa fa-close strip__close"><h1>×</h1></i>
     </section>
 
+    <!-- 時間の表示 -->
     <h1>
         <niko>
             <?php 
